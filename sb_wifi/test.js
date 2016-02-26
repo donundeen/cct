dc60c3ba03735ec50cc3b60b34073018c8884be0
don undeen <donundeen@gmail.com>
2016-02-26 18:08:34 +0000

    first add

---
+++ sb_wifi/test.js
@@ -0,0 +1,22 @@
+
+var SB= require("spacebrew");
+
+var sb_host = "sandbox.spacebrew.cc";
+
+var name ="CCT";
+
+var description = "iteractive tech";
+
+var sb  = new SB.Client(sb_host, name, description);
+
+sb.addPublish("tick", "string", "tick tock");
+var i = 0;
+
+sb.connect();
+
+setInterval(function(){
+    
+    sb.send("tick", "string", "listen up!" + i);
+    i++;
+    console.log('sent');
+}, 1000);
