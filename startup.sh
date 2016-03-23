d65f48d60a164e9af5651bd2c9b9a5fadf48b0a2
don undeen <donundeen@yahoo.com>
2016-03-23 15:55:27 -0400

    loggin

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin
 cd /home/pi/cct/sb_wifi
 /usr/bin/sudo /usr/bin/git pull
-/usr/local/bin/forever start /home/pi/cct/sb_wifi/serial_to_spacebrew.js >> /home/pi/cct/sb_wifi/serial_to_spacebrew.log
+/usr/local/bin/forever start -o /home/pi/cct/sb_wifi/serial_to_spacebrew_forever.log -e /home/pi/cct/sb_wifi/serial_to_spacebrew_forever_error.log /home/pi/cct/sb_wifi/serial_to_spacebrew.js >> /home/pi/cct/sb_wifi/serial_to_spacebrew.log
