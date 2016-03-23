0b4304a22ab39ad2bc0bdedabd2580e3687dd5f7
don undeen <donundeen@yahoo.com>
2016-03-23 15:34:42 -0400

    edits

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-//put in init.d
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin
+#put in init.d
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin
 sudo cd /home/pi/cct/sb_wifi
 sudo git pull
 forever start /home/pi/cct/sb_wifi/serial_to_spacebrew.js >> /home/pi/cct/sb_wifi/serial_to_spacebrew.log
